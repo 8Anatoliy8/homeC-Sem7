e9c1ff501aa92d00422df520e4d9a7323c4d335c
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-02-03 13:39:53 +0300

    complete Task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void ShowNumbers (int start, int end)
+{
+    if (start == end)
+    {
+        Console.Write(end);
+        return;
+    }
+    Console.Write(start + ", ");
+    ShowNumbers(start + 1, end);
+}
+
+ShowNumbers(M,N);
