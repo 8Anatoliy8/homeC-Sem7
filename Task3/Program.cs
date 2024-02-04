7c4feeb901d6360526ff5c49941e0f2ce8939d9d
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-02-04 10:44:25 +0300

    End

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,7 +7,7 @@ int [] CreateRandomArray (int size)
     Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rnd.Next (-101, 101);
+        array[i] = rnd.Next (-100, 101);
     }
     return array;
 }
@@ -18,12 +18,23 @@ void ShowReverseArray (int [] arr, int size)
     {
         return;
     }
-    Console.Write($"Перевернутый Массив:[{string.Join ("; ", arr[size-1])}]");
+    else if (size == arr.Length)
+    {
+        Console.Write($"Перевернутый массив: [{arr[size-1]}, ");
+    }
+    else if (size == 1)
+    {
+        Console.Write($"{arr[size-1]}]");
+    }
+    else
+    {
+        Console.Write(arr[size-1] + "; ");
+    }
     ShowReverseArray(arr, size-1);
 }
 
 Console.Write("Введите размер массива: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int [] array = CreateRandomArray (N);
-Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+Console.WriteLine($"Массив: [{string.Join("; ", array)}]");
 ShowReverseArray(array, N);
--- Task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4396b3a771a51afafe1407e32994df46aaf7b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c1e1b3bd579e33dd48e54cadb1eb90bf93c8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
